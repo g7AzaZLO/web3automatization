0cd3f40ea883823686265fa903aaf6d1579bd6b1
AzaZLO <MaloyMeee@yandex.ru>
2024-10-28 17:44:24 +0300

    fix crosscuve eth swap

--- web3automatization/modules/crosscurve/config.py
+++ web3automatization/modules/crosscurve/config.py
@@ -2,14 +2,14 @@ crosscurve_tokens = [
     {"chain": "ethereum", "ticker": "USDC", "address": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", "decimals": 6},
     {"chain": "ethereum", "ticker": "USDT", "address": "0xdac17f958d2ee523a2206206994597c13d831ec7", "decimals": 6},
     {"chain": "ethereum", "ticker": "DAI", "address": "0x6b175474e89094c44da98b954eedeac495271d0f", "decimals": 18},
-    {"chain": "ethereum", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "ethereum", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "ethereum", "ticker": "WETH", "address": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "decimals": 18},
     {"chain": "ethereum", "ticker": "WBTC", "address": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", "decimals": 8},
     {"chain": "ethereum", "ticker": "tBTC", "address": "0x18084fbA666a33d37592fA2633fD49a74DD93a88", "decimals": 18},
 
     {"chain": "arbitrum", "ticker": "USDC.e", "address": "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8", "decimals": 6},
     {"chain": "arbitrum", "ticker": "USDT", "address": "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9", "decimals": 6},
-    {"chain": "arbitrum", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "arbitrum", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "arbitrum", "ticker": "WETH", "address": "0x82af49447d8a07e3bd95bd0d56f35241523fbab1", "decimals": 18},
     {"chain": "arbitrum", "ticker": "WBTC", "address": "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f", "decimals": 8},
     {"chain": "arbitrum", "ticker": "tBTC", "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40", "decimals": 18},
@@ -17,7 +17,7 @@ crosscurve_tokens = [
     {"chain": "optimism", "ticker": "USDC.e", "address": "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", "decimals": 18},
     {"chain": "optimism", "ticker": "USDT", "address": "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58", "decimals": 6},
     {"chain": "optimism", "ticker": "DAI", "address": "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", "decimals": 18},
-    {"chain": "optimism", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "optimism", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "optimism", "ticker": "WETH", "address": "0x4200000000000000000000000000000000000006", "decimals": 18},
     {"chain": "optimism", "ticker": "WBTC", "address": "0x68f180fcCe6836688e9084f035309E29Bf0A2095", "decimals": 8},
     {"chain": "optimism", "ticker": "tBTC", "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40", "decimals": 18},
@@ -45,22 +45,22 @@ crosscurve_tokens = [
     {"chain": "base", "ticker": "axlUSDC", "address": "0xeb466342c4d449bc9f53a865d5cb90586f405215", "decimals": 6},
     {"chain": "base", "ticker": "crvUSD", "address": "0x417ac0e078398c154edfadd9ef675d30be60af93", "decimals": 18},
     {"chain": "base", "ticker": "USDC", "address": "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913", "decimals": 6},
-    {"chain": "base", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "base", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "base", "ticker": "WETH", "address": "0x4200000000000000000000000000000000000006", "decimals": 18},
 
     {"chain": "gnosis", "ticker": "USDC", "address": "0xddafbb505ad214d7b80b1f830fccc89b60fb7a83", "decimals": 6},
     {"chain": "gnosis", "ticker": "USDT", "address": "0x4ecaba5870353805a9f068101a40e0f32ed605c6", "decimals": 6},
     {"chain": "gnosis", "ticker": "WXDAI", "address": "0xe91d153e0b41518a2ce8dd3d7944fa863463a97d", "decimals": 18},
 
-    {"chain": "blast", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "blast", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "blast", "ticker": "WETH", "address": "0x4300000000000000000000000000000000000004", "decimals": 18},
 
     {"chain": "mantle", "ticker": "WETH", "address": "0xdEAddEaDdeadDEadDEADDEAddEADDEAddead1111", "decimals": 18},
 
-    {"chain": "linea", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "linea", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "linea", "ticker": "WETH", "address": "0xe5d7c2a44ffddf6b295a15c148167daaaf5cf34f", "decimals": 18},
 
-    {"chain": "taiko", "ticker": "ETH", "address": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "decimals": 18},
+    {"chain": "taiko", "ticker": "ETH", "address": "0x0000000000000000000000000000000000000000", "decimals": 18},
     {"chain": "taiko", "ticker": "WETH", "address": "0xa51894664a773981c6c112c43ce576f315d5b1b6", "decimals": 18},
 ]
 
