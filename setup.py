253ea2792216c5e9017cec6eaa0f04a051a74c63
AzaZLO <MaloyMeee@yandex.ru>
2024-10-30 18:35:15 +0300

    version 0.0.2 for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.0.1'
+version = '0.0.2'
 setup(
     name='web3automatization',
     version=version,
